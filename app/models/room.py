81f2507369d3cb64b04c2bb1907f171dec46a409
jwc20 <choi.jae100@gmail.com>
2025-07-24 13:43:51 +0900

    add created_by and create_at column to Room

--- app/models/room.py
+++ app/models/room.py
@@ -1,15 +1,17 @@
 from sqlmodel import Field, SQLModel
-
+from datetime import datetime, timezone
 
 class RoomBase(SQLModel):
     room_name: str = Field(index=True)
     max_players: int = Field(default=None)
     number_of_actions: int = Field(default=None)
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
     disabled: bool = Field(default=False)
-
+    
 
 class Room(RoomBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
+    created_by: int = Field(foreign_key="user.id", index=True)
     
     
     
