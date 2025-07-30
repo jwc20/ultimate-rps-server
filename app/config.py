c0768d91ce94223c9e163cbef9f11920ccd74ec7
jwc20 <choi.jae100@gmail.com>
2025-07-30 15:13:26 +0900

    add option to use postgres

--- app/config.py
+++ app/config.py
@@ -7,8 +7,10 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM =  os.getenv("ALGORITHM") 
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
 REDIS_URL = os.getenv("REDIS_URL")
 
-# DATABASE_URL="sqlite:///database.db"
-DATABASE_URL = os.getenv("DATABASE_URL")
+# SQLITE_URL="sqlite:///database.db"
+
+DEV = os.environ.get("DEV", "true").lower() == "true"
+SQLITE_URL = os.environ.get("SQLITE_URL", "sqlite:///./sql_app.db")
+POSTGRES_URL = os.environ.get("POSTGRES_URL")
